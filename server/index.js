6158c3c00cbb72e60ffb5a254e7f59bafa297a3a
Graciasc <gracias.claude@hotmail.com>
2019-11-13 20:20:45 -0700

    added nodemone to refresh server/ added ability to serve static page/ added html and css file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 app.listen(3000, () => {
     console.log('Server Started....')
 })
 
-
-app.use('/', (req,res) => {
-    res.send('Hello World')
+app.use('/static',express.static("public")); // serving static files
+app.use('/server', (req,res) => {// sending server world ---
+    res.send('Server World')
 })
 module.export = app;
