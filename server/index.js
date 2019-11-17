2dfcfb0220a26825b7b79d76568412297b899b1a
Graciasc <gracias.claude@hotmail.com>
2019-11-16 20:58:29 -0700

    figured out the routing the about page

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-// const about = require('../public/About')
 
 app.listen(3000, () => {
     console.log('Server Started....')
 })
 
-app.use('/static',express.static("public")); // serving static files
+app.use('/',express.static("public")); // serving static files
+app.use('/about.html',express.static("public")); // serving static files
 app.use('/server', (req,res) => {// sending server world ---
     res.send('Server World')
 })
-app.use('/about', express.static('about'))
 module.export = app;
