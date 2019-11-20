7906cf787f90474ce791091a21b607e9441cdb50
gracias <gclaude@endurance.com>
2019-11-19 18:09:48 -0700

    successfully created a post request and updated the About.html textarea to a Form added bodyParser

--- server/index.js
+++ server/index.js
@@ -1,18 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const routes = require('./routes');
+const path = require("path");
+const bodyParser = require("body-parser");
 
-app.listen(3000, () => {
-  console.log('Server Started....');
-});
 // app.use('/generic', routes);
 // app.get('/')
-app.use('/', express.static('public')); // serving static files
-app.use('/about.html', express.static('public')); // serving static files
-app.use('/server', (req, res) => {
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+app.use("/", express.static("public")); // serving static files
+app.use("/about.html", express.static("public")); // serving static files
+app.use("/server", (req, res) => {
   // sending server world ---
-  res.send('Server World');
-//   res.writeHead(200);
+  res.send("Server World");
+  //   res.writeHead(200);
+});
+app.post("/about.html", (req, res) => {
+  res.send(`post request for feedback: FeedBack written: ${req.body.feed}`);
+  console.log(req.body.userfeed);
+});
+app.listen(3000, () => {
+  console.log("Server Started....");
 });
 module.export = app;
