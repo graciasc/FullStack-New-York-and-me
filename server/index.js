97c47b3d072ef707395ec3b01f099100ed05a1fa
gracias <gclaude@endurance.com>
2019-11-22 18:02:41 -0700

    post is not sending to the database because only getting one form input

--- server/index.js
+++ server/index.js
@@ -2,9 +2,29 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+mongoose.connect(
+  "mongodb+srv://gman:1@learning-cluster-jurht.mongodb.net/test?retryWrites=true",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-// app.use('/generic', routes);
-// app.get('/')
+// .then(() => {
+//   console.log("Connected to Database");
+//   }).catch((err) => {
+//       console.log("Not Connected to Database ERROR! ", err);
+//   });
+// console.log(process.env.MONGO_URI)
+
+//created a nenw schema for a person
+const feedbackSchema = new Schema({
+  name: { type: String, required: true },
+  textInput: { type: String, required: true }
+});
+
+const Feed = mongoose.model("Feedback", feedbackSchema);
+
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true
@@ -19,8 +39,18 @@ app.use("/server", (req, res) => {
   //   res.writeHead(200);
 });
 app.post("/about.html", (req, res) => {
-  res.send(`post request for feedback: FeedBack written: ${req.body.feed}`);
-  console.log(req.body.userfeed);
+  // beilieve this is saving the body of the post request
+  const myData = new Feed(req.body);
+  myData
+    .save()
+    .then(item => {
+      res.send("item saved to database");
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
+  // res.send(`post request for feedback: FeedBack written: ${req.body.feed}`);
+  console.log(req.body.feed, req.body.name);
 });
 app.listen(3000, () => {
   console.log("Server Started....");
