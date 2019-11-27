385c7ff7a7b3772c46350a64ead9e9372d1210ce
gracias <gclaude@endurance.com>
2019-11-27 09:17:11 -0700

    removing db uri

--- server/index.js
+++ server/index.js
@@ -10,8 +10,6 @@ mongoose.connect(
   process.env.MONGO_URI,
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
-// mongodb+srv://gman:1@learning-cluster-jurht.mongodb.net/test?retryWrites=true
-console.log(process.env.MONGO_URI)
 
 // .then(() => {
 //   console.log("Connected to Database");
